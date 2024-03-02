90fee8dbbaca8e83e4e7ad8b399ac034b78a83a3
Evgeni Glushko <sstawr@gmail.com>
2024-03-02 05:09:44 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Evgeni Glushko on 2.03.24.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
