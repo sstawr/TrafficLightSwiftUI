a37591dac0363064260d299b6d5870a690c92976
Evgeni Glushko <sstawr@gmail.com>
2024-03-02 05:46:24 +0300

    UI Done

--- TrafficLight/BulbView.swift
+++ TrafficLight/BulbView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct BulbView: View {
+    let color: Color
+    let opacity: Double
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+            .foregroundColor(color)
+            .opacity(opacity)
+            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.cyan, lineWidth: 4))
+            .padding(5)
     }
 }
 
 #Preview {
-    BulbView()
+    BulbView(color: .red, opacity: 0.3)
 }
--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+    
+    private let currentLight = CurrentLight.red
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                BulbView(color: .red, opacity: 0.3)
+                BulbView(color: .yellow, opacity: 0.3)
+                BulbView(color: .green, opacity: 0.3)
+            }
+            
+            Spacer()
+            
+            Button(action: buttonAction) {
+                Text("Run")
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+            }
+            .frame(width: 150, height: 50)
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 25))
+            .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color.cyan, lineWidth: 4))
+            
         }
         .padding()
+    }
+    
+    private func buttonAction() {
+        
     }
 }
 
