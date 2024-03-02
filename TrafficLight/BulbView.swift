658fbc4700b832979e97c473e2a9956b30955dfc
Evgeni Glushko <sstawr@gmail.com>
2024-03-02 05:46:12 +0300

    UI Done

---
+++ TrafficLight/BulbView.swift
@@ -0,0 +1,18 @@
+//
+//  BulbView.swift
+//  TrafficLight
+//
+//  Created by Evgeni Glushko on 2.03.24.
+//
+
+import SwiftUI
+
+struct BulbView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BulbView()
+}
